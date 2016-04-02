2be8bb5884cd30480a2d02c5c4159b3b8634f674
Brian M. Waters <brian@brianmwaters.net>
2016-04-02 09:21:22 -0400

    Comment model

--- eventsapp/models.py
+++ eventsapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey("UVMUser")
+    event = models.ForeignKey("Event")
+    content = models.TextField()
+    time = models.DateTimeField(auto_now=True)
