990368deca2ba0ba2fcc3f7f42c1fe2db64b66bd
Brian M. Waters <brian@brianmwaters.net>
2016-04-02 12:28:32 -0400

    do a migration merge thingy

---
+++ eventsapp/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('eventsapp', '0003_remove_club_events'),
+        ('eventsapp', '0003_auto_20160402_1559'),
+    ]
+
+    operations = [
+    ]
