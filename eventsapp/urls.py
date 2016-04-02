aede117d25e3bf2d3d137c4086bd75f87c7d5d87
Brian M. Waters <brian@brianmwaters.net>
2016-04-02 10:49:04 -0400

    Account page w/ no functionality

--- eventsapp/urls.py
+++ eventsapp/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'eventsapp/login.html'}),
     url(r'^accounts/logout/$', 'eventsapp.views.logout_view'),
-    url(r'^$', 'eventsapp.views.home_view')
+    url(r'^$', 'eventsapp.views.home_view'),
+    url(r'^account$', 'eventsapp.views.account_view')
 )
