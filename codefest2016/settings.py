866789f01c006c89579976ec8da90bb04037eb91
Dillon Beliveau <dillonbeliveau@gmail.com>
2016-04-01 20:19:21 -0400

    Added app

--- codefest2016/settings.py
+++ codefest2016/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'codefest2016'
+    'eventsapp'
 ]
 
 MIDDLEWARE_CLASSES = [
