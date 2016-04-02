fa09d3c9c0f134d3f6abf5405f87d25c5d32e5b2
Dillon Beliveau <dillonbeliveau@gmail.com>
2016-04-02 09:33:44 -0400

    Ldap

--- eventsapp/models.py
+++ eventsapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+
+class UVMUserManager(BaseUserManager):
+    def _create_user(self, netid, password, first_name=None, last_name=None, uvm_email=None, full_name=None, department=None):
+        if not netid:
+            raise ValueError("Netid must be set")
+
+        user = self.model(netid=netid, first_name=first_name, last_name=last_name, full_name=full_name, department=department)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, netid, password=None, **extra_fields):
+        return self._create_user(netid,password, **extra_fields)
+
+    def create_superuser(self, netid, password=None, **extra_fields):
+        return self._create_user(netid, password, **extra_fields)
+
+class UVMUser(AbstractBaseUser):
+    """A custom user model to represent a UVM student."""
+    netid = models.CharField(max_length=8,unique=True)
+    first_name = models.CharField(max_length=40,null=True)
+    last_name = models.CharField(max_length=40,null=True)
+    uvm_email = models.CharField(max_length=40,null=True)
+    full_name = models.CharField(max_length=150,null=True)
+    department = models.CharField(max_length=40,null=True)
+
+    facebook_url = models.CharField(max_length=100, null=True)
+    additional_email_1 = models.CharField(max_length=100, null=True)
+    additional_email_2 = models.CharField(max_length=100, null=True)
+    phone_number = models.CharField(max_length=14, null=True)
+    #user_picture goes here later
+
+    USERNAME_FIELD = 'netid'
+    objects = UVMUserManager()
