50b5291547041f6e71e478d726f5636d6807bf52
Dillon Beliveau <dillonbeliveau@gmail.com>
2016-04-02 10:20:51 -0400

    Fixed a few template errors

--- eventsapp/urls.py
+++ eventsapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'eventsapp/home.html'}),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'eventsapp/login.html'}),
     url(r'^accounts/logout/$', 'eventsapp.views.logout_view'),
     url(r'^$', 'eventsapp.views.home_view')
 )
